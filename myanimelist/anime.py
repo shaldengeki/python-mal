44e08e2e0f9f898ef3379237152fcb506d5c4cc6
Charles Guo <shaldengeki@gmail.com>
2014-03-22 22:37:36 -0500

    Add month-year pairing to parse_date

--- myanimelist/anime.py
+++ myanimelist/anime.py
@@ -35,16 +35,20 @@ def parse_date(text):
   """
     Parses a MAL date on an anime page.
     May raise ValueError if a malformed date is found.
-    If text is "Unknown" or "?" then returns None.
+    If text is "Unknown" or "?" or "Not available" then returns None.
     Otherwise, returns a datetime.date object.
   """
-  if text == "Unknown" or text == "?":
+  if text == u"Unknown" or text == u"?" or text == u"Not available":
     return None
   try:
     aired_date = datetime.datetime.strptime(text, '%Y').date()
   except ValueError:
     # see if it's a date.
-    aired_date = datetime.datetime.strptime(text, '%b %d, %Y').date()
+    try:
+      aired_date = datetime.datetime.strptime(text, '%b %d, %Y').date()
+    except ValueError:
+      # see if it's a month/year pairing.
+      aired_date = datetime.datetime.strptime(text, '%b %Y').date()
   return aired_date
 
 class Anime(Base):
